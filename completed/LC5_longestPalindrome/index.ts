6cd26454b37c479b6a91142a3f1e4e06b3255387
zana-abdollahpour <zana.ap4@gmail.com>
2024-03-31 15:07:15 +0330

    LC5_longestPalindrome

---
+++ completed/LC5_longestPalindrome/index.ts
@@ -0,0 +1,31 @@
+//Given a string s, find the longest palindromic substring in s
+// --- Example
+// longestPalindrome("cbbd") --> "bb"
+// longestPalindrome("abba") --> "abba"
+// longestPalindrome("a") --> "a"
+
+function longestPalindrome(s: string): string {
+  let startIndex = 0;
+  let maxLength = 1;
+
+  function expandAroundMiddle(left: number, right: number) {
+    while (left >= 0 && right < s.length && s[left] === s[right]) {
+      const curPalLength = right - left + 1;
+      if (curPalLength > maxLength) {
+        maxLength = curPalLength;
+        startIndex = left;
+      }
+      left -= 1;
+      right += 1;
+    }
+  }
+
+  for (let i = 0; i < s.length; i++) {
+    expandAroundMiddle(i - 1, i + 1);
+    expandAroundMiddle(i, i + 1);
+  }
+
+  return s.slice(startIndex, startIndex + maxLength);
+}
+
+module.exports = longestPalindrome;
