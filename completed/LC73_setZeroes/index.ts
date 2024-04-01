bd99611466e1f42f01d313ee4641928e0d368127
zana-abdollahpour <zana.ap4@gmail.com>
2024-04-01 22:08:06 +0330

    LC73_setZeroes

---
+++ completed/LC73_setZeroes/index.ts
@@ -0,0 +1,54 @@
+function setZeroes(matrix: number[][]) {
+  let firstColHasZero = false;
+  let firstRowHasZero = false;
+
+  // Check if first col has zero
+  for (let i = 0; i < matrix.length; i++) {
+    if (matrix[i][0] === 0) {
+      firstColHasZero = true;
+      break;
+    }
+  }
+
+  // Check if first row has zero
+  for (let i = 0; i < matrix[0].length; i++) {
+    if (matrix[0][i] === 0) {
+      firstRowHasZero = true;
+      break;
+    }
+  }
+
+  // Use first row and col as flags, if rest off cells have zeros
+  for (let row = 1; row < matrix.length; row++) {
+    for (let col = 1; col < matrix[0].length; col++) {
+      if (matrix[row][col] === 0) {
+        matrix[0][col] = 0;
+        matrix[row][0] = 0;
+      }
+    }
+  }
+
+  // Zero out cells based on flags in first row and column
+  for (let row = 1; row < matrix.length; row++) {
+    for (let col = 1; col < matrix[0].length; col++) {
+      if (matrix[row][0] === 0 || matrix[0][col] === 0) {
+        matrix[row][col] = 0;
+      }
+    }
+  }
+
+  // Zero out first column
+  if (firstColHasZero) {
+    for (let i = 0; i < matrix.length; i++) {
+      matrix[i][0] = 0;
+    }
+  }
+  // Zero out first row
+  if (firstRowHasZero) {
+    for (let i = 0; i < matrix[0].length; i++) {
+      matrix[0][i] = 0;
+    }
+  }
+}
+
+module.exports = setZeroes;
