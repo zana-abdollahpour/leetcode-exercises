4d85c3be563ca280396c4cdc3a4b345d6df62d35
zana-abdollahpour <zana.ap4@gmail.com>
2024-04-01 15:10:33 +0330

    LC15_threeSum

---
+++ completed/LC15_threeSum/index.ts
@@ -0,0 +1,30 @@
+const threeSum = (nums: number[]) => {
+  nums.sort((a, b) => a - b);
+  let res: Array<number[]> = [];
+
+  for (let i = 0; i < nums.length; i++) {
+    const target = 0 - nums[i];
+    let left = i + 1;
+    let right = nums.length - 1;
+
+    if (i > 0 && nums[i] === nums[i - 1]) continue;
+
+    while (left < right) {
+      if (nums[left] + nums[right] === target) {
+        res.push([nums[i], nums[left], nums[right]]);
+        left++;
+        while (nums[left] === nums[left - 1]) {
+          left++;
+        }
+      } else if (nums[left] + nums[right] < target) {
+        left++;
+      } else {
+        right--;
+      }
+    }
+  }
+
+  return res;
+};
+
+module.exports = threeSum;
