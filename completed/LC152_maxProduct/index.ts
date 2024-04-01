04f185ad3291ac93d9c74fec00e4bceeb670e443
zana-abdollahpour <zana.ap4@gmail.com>
2024-04-01 15:38:36 +0330

    LC152_maxProduct

---
+++ completed/LC152_maxProduct/index.ts
@@ -0,0 +1,27 @@
+function maxProduct(nums: number[]) {
+  let maxTillIndex = [nums[0]];
+  let minTillIndex = [nums[0]];
+  let max = nums[0];
+
+  for (let i = 1; i < nums.length; i++) {
+    const num = nums[i];
+
+    maxTillIndex[i] = Math.max(
+      num,
+      num * maxTillIndex[i - 1],
+      num * minTillIndex[i - 1]
+    );
+
+    minTillIndex[i] = Math.min(
+      num,
+      num * maxTillIndex[i - 1],
+      num * minTillIndex[i - 1]
+    );
+
+    max = Math.max(max, maxTillIndex[i]);
+  }
+
+  return max;
+}
+
+module.exports = maxProduct;
