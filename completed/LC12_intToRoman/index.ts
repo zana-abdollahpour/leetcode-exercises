da84222809f6a25db7a2ff35f76dac456e501018
zana-abdollahpour <zana.ap4@gmail.com>
2024-04-12 15:33:59 +0330

    LC12_intToRoman

---
+++ completed/LC12_intToRoman/index.ts
@@ -0,0 +1,45 @@
+function intToRoman(num: number): string {
+  const map: Record<string, string> = {
+    1: "I",
+    4: "IV",
+    5: "V",
+    9: "IX",
+    10: "X",
+    40: "XL",
+    50: "L",
+    90: "XC",
+    100: "C",
+    400: "CD",
+    500: "D",
+    900: "CM",
+    1000: "M",
+  };
+
+  let results = "";
+  const checker = [10, 100, 1000, 10000];
+  for (let i = 0; i < checker.length, num > 0; i++) {
+    const found = num % checker[i];
+
+    if (map[found]) {
+      results = map[found] + results;
+      num -= found;
+      continue;
+    }
+    const uniqe = checker[i] / 10;
+    for (let j = found; j > 0; j -= uniqe) {
+      if (map[j]) {
+        results = map[j] + results;
+        break;
+      } else {
+        results = map[uniqe] + results;
+      }
+    }
+    num -= found;
+  }
+
+  return results;
+}
+
+console.log(intToRoman(1994));
+console.log(intToRoman(58));
+console.log(intToRoman(3));
