ba8913563eef68aacc9b0e5be407e9a2568b830d
zana-abdollahpour <zana.ap4@gmail.com>
2024-04-15 14:19:01 +0330

    LC83_deleteDuplicates

---
+++ completed/LC83_deleteDuplicates/index.ts
@@ -0,0 +1,23 @@
+class ListNode {
+  val: number;
+  next: ListNode | null;
+  constructor(val?: number, next?: ListNode | null) {
+    this.val = val === undefined ? 0 : val;
+    this.next = next === undefined ? null : next;
+  }
+}
+
+function deleteDuplicates(head: ListNode | null): ListNode | null {
+  let node = head;
+
+  while (node !== null) {
+    if (node.next !== null)
+      if (node.val === node.next.val) {
+        node.next = node.next.next;
+        continue;
+      }
+    node = node.next;
+  }
+
+  return head;
+}
