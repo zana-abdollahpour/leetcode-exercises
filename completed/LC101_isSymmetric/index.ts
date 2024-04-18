34cf62de0afd224f06adfe4119a95c210b7c66e8
zana-abdollahpour <zana.ap4@gmail.com>
2024-04-18 13:37:15 +0330

    LC101_isSymmetric

---
+++ completed/LC101_isSymmetric/index.ts
@@ -0,0 +1,27 @@
+class TreeNode {
+  val: number;
+  left: TreeNode | null;
+  right: TreeNode | null;
+  constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+    this.val = val === undefined ? 0 : val;
+    this.left = left === undefined ? null : left;
+    this.right = right === undefined ? null : right;
+  }
+}
+
+const isSymmetric = (root: TreeNode | null): boolean => {
+  const isMirror = (t1: TreeNode | null, t2: TreeNode | null): boolean => {
+    if (t1 === null && t2 === null) return true;
+    if (t1 === null || t2 === null) return false;
+
+    return (
+      t1.val === t2.val &&
+      isMirror(t1.left, t2.right) &&
+      isMirror(t1.right, t2.left)
+    );
+  };
+
+  return isMirror(root, root);
+};
+
+export default isSymmetric;
