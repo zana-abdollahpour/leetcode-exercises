ca754077b9525a8aed10f1c1f99711af9aecca8e
zana-abdollahpour <zana.ap4@gmail.com>
2024-07-29 10:57:01 +0330

    fix small bug in LC136_singleNumber

--- completed/LC136_singleNumber/index.ts
+++ completed/LC136_singleNumber/index.ts
@@ -1,7 +1,9 @@
-function singleNumber(nums: number[]): number | void {
+function singleNumber(nums: number[]): number {
   const sortedArr = nums.sort((a, b) => a - b);
   for (let i = 0; i < sortedArr.length; i = i + 2)
     if (sortedArr[i] !== sortedArr[i + 1]) return sortedArr[i];
+
+  return nums[nums.length - 1];
 }
 
 console.log(singleNumber([4, 1, 2, 1, 2]));
