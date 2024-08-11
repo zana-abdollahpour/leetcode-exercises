1916b3926479b3b081791fde7194a8bda8c16a56
zana-abdollahpour <zana.ap4@gmail.com>
2024-08-11 21:32:00 +0330

    LC303_sumRange

---
+++ completed/301-350/LC303_sumRange/index.ts
@@ -0,0 +1,16 @@
+class NumArray {
+  constructor(public nums: number[]) {}
+
+  sumRange(left: number, right: number): number {
+    let sum = 0;
+    for (let i = left; i <= right; i++) sum += this.nums[i];
+
+    return sum;
+  }
+}
+
+/**
+ * Your NumArray object will be instantiated and called as such:
+ * var obj = new NumArray(nums)
+ * var param_1 = obj.sumRange(left,right)
+ */
