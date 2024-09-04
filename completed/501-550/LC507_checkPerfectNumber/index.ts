6494a6e818dc8b9d2003e46b38119f0966bf64a3
zana-abdollahpour <zana.ap4@gmail.com>
2024-09-04 09:55:33 +0330

    LC507_checkPerfectNumber

---
+++ completed/501-550/LC507_checkPerfectNumber/index.ts
@@ -0,0 +1,10 @@
+function checkPerfectNumber(num: number): boolean {
+  if (num % 2 === 1) return false;
+
+  let sum = 0;
+  for (let i = 1; i <= num / 2; i++) {
+    if (num % i === 0) sum += i;
+  }
+
+  return sum === num;
+}
